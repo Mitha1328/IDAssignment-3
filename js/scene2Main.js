6e5144d76f46b7f2100ab4c6afa241641d238bcd
j6ine <65351853+j6ine@users.noreply.github.com>
2021-02-16 17:11:51 +0800

    scene 2 need few more changes.

--- js/scene2Main.js
+++ js/scene2Main.js
@@ -1,29 +1,28 @@
 $(document).ready(function () {
-    //hide parts to be shown only when button is clicked
-    $("#part-2").hide();
-    $("#part-3").hide();
-    $("#part-4").hide();
-  
-    // "btn-next-1" will go to "part-2" ...
-    //event listener for button
-    $(".btn-next-1").on("click", function (e) {
-      $("#part-2").show();
-  
-    });
-  
-    $(".btn-next-2").on("click", function (e) {
-      $(".time-travel-anim").html(`
-      <lottie-player src="https://assets5.lottiefiles.com/temp/lf20_kDSNzl.json"  background="transparent"  speed="1"  style="width: 100%; height: 100%;"    autoplay loop></lottie-player>`);
-  
-      $("#part-3").show();
-      
-    });
-  
-    $(".btn-next-3").on("click", function (e) {
-      $("#part-4").show();
-      
-    });
+  //hide parts to be shown only when button is clicked
+  $("#part-2").hide();
+  $("#part-3").hide();
+  $("#part-4").hide();
+
+  // "btn-next-1" will go to "part-2" ...
+  //event listener for button
+  $(".btn-next-1").on("click", function (e) {
+    $("#part-2").show();
+
+  });
+
+  $(".btn-next-2").on("click", function (e) {
+    $(".time-travel-anim").html(`
+    <lottie-player src="https://assets5.lottiefiles.com/temp/lf20_kDSNzl.json"  background="transparent"  speed="1"  style="width: 100%; height: 100%;"    autoplay loop></lottie-player>`);
+
+    $("#part-3").show();
     
+  });
+
+  $(".btn-next-3").on("click", function (e) {
+    $("#part-4").show();
     
   });
   
+  
+});
--- js/scene2Part3.js
+++ js/scene2Part3.js
@@ -1,121 +1,120 @@
 //part 3: story of the previous the night (why mary got sick)
 
 $(document).ready(function () {
-    //hide parts
-    $(".question-pt3-2").hide();
-    $(".question-pt3-3").hide();
-    $(".question-pt3-4").hide();
-    $(".question-pt3-5").hide();
-    $("#text-pt3-conclusion").hide();
-  
-  
-    //show what mary is doing at (1) 8pm, (2) 9pm, (3) 12pm, (4) 4am, (5) 6am
-  
-    //(1) 8pm
-    $(".btn-pt3-1").on("click", function (e) {
-      var text = `
-      <div class="row">
-          <div class="col-8">
-            <p class="robot-speech" style="margin-top: 20%;">
-              8pm: Mary just bought a new game! She has been waiting in line for 3 hours to get it.
-            </p> 
-          </div>
-  
-          <div class="col-4">
-            <lottie-player src="https://assets6.lottiefiles.com/private_files/lf30_w5puR3.json"  background="transparent"  speed="1"  style="width: 100%; height: 100%;"    autoplay></lottie-player>
-          </div>
-      </div>
-      `;
-      
-      $("#text-pt3-1").html(text);
-      $(".question-pt3-2").show();
-  
-    });
-  
-    //(2) 9pm
-    $(".btn-pt3-2").on("click", function (e) {
-      var text = `
-      <div class="row">
-          <div class="col-8">
-            <p class="robot-speech" style="margin-top: 20%;">
-              8pm: Mary just bought a new game! She has been waiting in line for 3 hours to get it.
-            </p> 
-          </div>
-  
-          <div class="col-4">
-            <lottie-player src="https://assets6.lottiefiles.com/private_files/lf30_w5puR3.json"  background="transparent"  speed="1"  style="width: 100%; height: 100%;"    autoplay></lottie-player>
-          </div>
-      </div>
-      `;
-       
-      $("#text-pt3-2").html(text);
-      $(".question-pt3-3").show();
-      
-    });
-  
-    //(3) 12am
-    $(".btn-pt3-3").on("click", function (e) {
-      var text = `
-      <div class="row">
-          <div class="col-8">
-            <p class="robot-speech" style="margin-top: 20%;">
-              8pm: Mary just bought a new game! She has been waiting in line for 3 hours to get it.
-            </p> 
-          </div>
-  
-          <div class="col-4">
-            <lottie-player src="https://assets6.lottiefiles.com/private_files/lf30_w5puR3.json"  background="transparent"  speed="1"  style="width: 100%; height: 100%;"    autoplay></lottie-player>
-          </div>
-      </div>
-      `;
-      
-      $("#text-pt3-3").html(text);
-      $(".question-pt3-4").show();
-      
-    });
-  
-    //(4) 4am
-    $(".btn-pt3-4").on("click", function (e) {
-      var text = `
-      <div class="row">
-          <div class="col-8">
-            <p class="robot-speech" style="margin-top: 20%;">
-              8pm: Mary just bought a new game! She has been waiting in line for 3 hours to get it.
-            </p> 
-          </div>
-  
-          <div class="col-4">
-            <lottie-player src="https://assets6.lottiefiles.com/private_files/lf30_w5puR3.json"  background="transparent"  speed="1"  style="width: 100%; height: 100%;"    autoplay></lottie-player>
-          </div>
-      </div>
-      `;
-      
-      $("#text-pt3-4").html(text);
-      $(".question-pt3-5").show();
-  
-    });
-  
-    //(5) 6am
-    $(".btn-pt3-5").on("click", function (e) {
-      var text = `
-      <div class="row">
-          <div class="col-8">
-            <p class="robot-speech" style="margin-top: 20%;">
-              8pm: Mary just bought a new game! She has been waiting in line for 3 hours to get it.
-            </p> 
-          </div>
-  
-          <div class="col-4">
-            <lottie-player src="https://assets6.lottiefiles.com/private_files/lf30_w5puR3.json"  background="transparent"  speed="1"  style="width: 100%; height: 100%;"    autoplay></lottie-player>
-          </div>
-      </div>
-      `;
-      
-      $("#text-pt3-5").html(text);
-      $("#text-pt3-conclusion").show();
-      
-    });
-  
-  
+  //hide parts
+  $(".question-pt3-2").hide();
+  $(".question-pt3-3").hide();
+  $(".question-pt3-4").hide();
+  $(".question-pt3-5").hide();
+  $("#text-pt3-conclusion").hide();
+
+
+  //show what mary is doing at (1) 8pm, (2) 9pm, (3) 12pm, (4) 4am, (5) 6am
+
+  //(1) 8pm
+  $(".btn-pt3-1").on("click", function (e) {
+    var text = `
+    <div class="row">
+        <div class="col-8">
+          <p class="robot-speech" style="margin-top: 20%;">
+            8pm: Mary just bought a new game! She has been waiting in line for 3 hours to get it.
+          </p> 
+        </div>
+
+        <div class="col-4">
+          <lottie-player src="https://assets6.lottiefiles.com/private_files/lf30_w5puR3.json"  background="transparent"  speed="1"  style="width: 100%; height: 100%;"    autoplay></lottie-player>
+        </div>
+    </div>
+    `;
+    
+    $("#text-pt3-1").html(text);
+    $(".question-pt3-2").show();
+
   });
-  
+
+  //(2) 9pm
+  $(".btn-pt3-2").on("click", function (e) {
+    var text = `
+    <div class="row">
+        <div class="col-8">
+          <p class="robot-speech" style="margin-top: 20%;">
+            8pm: Mary just bought a new game! She has been waiting in line for 3 hours to get it.
+          </p> 
+        </div>
+
+        <div class="col-4">
+          <lottie-player src="https://assets6.lottiefiles.com/private_files/lf30_w5puR3.json"  background="transparent"  speed="1"  style="width: 100%; height: 100%;"    autoplay></lottie-player>
+        </div>
+    </div>
+    `;
+     
+    $("#text-pt3-2").html(text);
+    $(".question-pt3-3").show();
+    
+  });
+
+  //(3) 12am
+  $(".btn-pt3-3").on("click", function (e) {
+    var text = `
+    <div class="row">
+        <div class="col-8">
+          <p class="robot-speech" style="margin-top: 20%;">
+            8pm: Mary just bought a new game! She has been waiting in line for 3 hours to get it.
+          </p> 
+        </div>
+
+        <div class="col-4">
+          <lottie-player src="https://assets6.lottiefiles.com/private_files/lf30_w5puR3.json"  background="transparent"  speed="1"  style="width: 100%; height: 100%;"    autoplay></lottie-player>
+        </div>
+    </div>
+    `;
+    
+    $("#text-pt3-3").html(text);
+    $(".question-pt3-4").show();
+    
+  });
+
+  //(4) 4am
+  $(".btn-pt3-4").on("click", function (e) {
+    var text = `
+    <div class="row">
+        <div class="col-8">
+          <p class="robot-speech" style="margin-top: 20%;">
+            8pm: Mary just bought a new game! She has been waiting in line for 3 hours to get it.
+          </p> 
+        </div>
+
+        <div class="col-4">
+          <lottie-player src="https://assets6.lottiefiles.com/private_files/lf30_w5puR3.json"  background="transparent"  speed="1"  style="width: 100%; height: 100%;"    autoplay></lottie-player>
+        </div>
+    </div>
+    `;
+    
+    $("#text-pt3-4").html(text);
+    $(".question-pt3-5").show();
+
+  });
+
+  //(5) 6am
+  $(".btn-pt3-5").on("click", function (e) {
+    var text = `
+    <div class="row">
+        <div class="col-8">
+          <p class="robot-speech" style="margin-top: 20%;">
+            8pm: Mary just bought a new game! She has been waiting in line for 3 hours to get it.
+          </p> 
+        </div>
+
+        <div class="col-4">
+          <lottie-player src="https://assets6.lottiefiles.com/private_files/lf30_w5puR3.json"  background="transparent"  speed="1"  style="width: 100%; height: 100%;"    autoplay></lottie-player>
+        </div>
+    </div>
+    `;
+    
+    $("#text-pt3-5").html(text);
+    $("#text-pt3-conclusion").show();
+    
+  });
+
+
+});
